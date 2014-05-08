d7479c9682bf245978aa11073e95bcb36ea550bb
Eugene Chen <eugenech@mit.edu>
2014-05-08 19:28:39 -0300

    Fixed Part 3. Rejects zero price, raises error properly, takes integer prices.

--- part3.rb
+++ part3.rb
@@ -2,13 +2,16 @@ class BookInStock
   attr_accessor :isbn, :price
 
   def initialize(isbn, price)
-    raise "ArgumentError" if isbn.length == 0 || price < 0
+    raise ArgumentError if isbn.length == 0 || price <= 0
     @isbn = isbn
     @price = price
   end
 
   def price_as_string
     ans = "$" << @price.to_s
+    if ans.index('.') == nil
+      ans << '.'
+    end
     little_digits = ans.length - ans.index('.') - 1
     until little_digits == 2 do
       ans << '0'
