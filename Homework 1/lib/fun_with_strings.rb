d1ef29e37e92a2ad516445b024ea67fa45ec9194
Eugene Chen <eugenech@mit.edu>
2014-05-17 20:29:23 -0300

    Finished fun_with_strings

--- Homework 1/lib/fun_with_strings.rb
+++ Homework 1/lib/fun_with_strings.rb
@@ -1,12 +1,16 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    stripped = self.downcase.gsub(/[^[[:word:]]\s]/, '')
+    stripped = stripped.gsub(/[ ]/, '')
+    return stripped == stripped.reverse
   end
   def count_words
-    # your code here
+    hash = Hash.new(0)
+    self.downcase.gsub(/[^[[:word:]]\s]/, '').split.each { |word| hash[word] += 1 }
+    return hash
   end
   def anagram_groups
-    # your code here
+    return self.split.group_by { |word| word.chars.sort.join }.values
   end
 end
 
