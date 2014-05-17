0d31df2d10789cc749b9f92b5af1babb95f01fc6
Eugene Chen <eugenech@mit.edu>
2014-05-17 20:53:39 -0300

    Added rock_paper_scissors

--- Homework 1/lib/rock_paper_scissors.rb
+++ Homework 1/lib/rock_paper_scissors.rb
@@ -4,11 +4,16 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    legal = ["R", "P", "S"]
+    wins  = ["P", "S", "R"]
+    raise RockPaperScissors::NoSuchStrategyError, "Strategy must be one of R,P,S" if !legal.include?(player1[1]) || !legal.include?(player2[1])
+    return player2 if player2[1] == wins[legal.index(player1[1])]
+    return player1
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    return self.winner(tournament[0], tournament[1]) if tournament[0][0].instance_of?(String)
+    return self.tournament_winner([self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1])])
   end
 
 end
